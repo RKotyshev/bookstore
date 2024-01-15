7786b677128078aa43f2cfef729a2efe975655a1
Roman Kotyshev <roman@MacBook-Air-Roman.local>
2024-01-15 17:06:39 +0300

    feat(elements): create events and topics components, change theming

--- src/app/feature/main-page/main-page.module.ts
+++ src/app/feature/main-page/main-page.module.ts
@@ -1,13 +1,23 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { MainPageComponent } from './main-page.component';
+import { EventsComponent } from './events/events.component';
+import { TopicsComponent } from './topics/topics.component';
+import { MatCardModule } from '@angular/material/card';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
 
 @NgModule({
   declarations: [
     MainPageComponent,
+    EventsComponent,
+    TopicsComponent,
   ],
   imports: [
     CommonModule,
+    MatCardModule,
+    MatButtonModule,
+    MatIconModule,
   ],
   exports: [
     MainPageComponent,
