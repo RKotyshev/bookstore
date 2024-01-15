cc2e2d7db7108481a4027f50a44858702d90ef54
Roman Kotyshev <roman@MacBook-Air-Roman.local>
2024-01-15 16:37:45 +0300

    feat(elements): create feature/main module and component, set up tsconfig

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,6 +9,7 @@ import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatIconModule } from '@angular/material/icon';
 import { MatButtonModule } from '@angular/material/button';
 import { MatListModule } from '@angular/material/list';
+import { MainPageModule } from './feature/main-page/main-page.module';
 
 @NgModule({
   declarations: [
@@ -23,6 +24,7 @@ import { MatListModule } from '@angular/material/list';
     MatIconModule,
     MatButtonModule,
     MatListModule,
+    MainPageModule,
   ],
   providers: [],
   bootstrap: [AppComponent],
