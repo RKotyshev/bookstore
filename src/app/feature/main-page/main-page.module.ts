848cda75ade3f8e639d2f13e67633cd6f6ef9f07
Roman Kotyshev <roman@MacBook-Air-Roman.local>
2024-01-16 15:22:52 +0300

    feat(elements): create novelties component, set up cdk layout module

--- src/app/feature/main-page/main-page.module.ts
+++ src/app/feature/main-page/main-page.module.ts
@@ -6,18 +6,22 @@ import { TopicsComponent } from './topics/topics.component';
 import { MatCardModule } from '@angular/material/card';
 import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
+import { NoveltiesComponent } from './novelties/novelties.component';
+import { LayoutModule } from '@angular/cdk/layout';
 
 @NgModule({
   declarations: [
     MainPageComponent,
     EventsComponent,
     TopicsComponent,
+    NoveltiesComponent,
   ],
   imports: [
     CommonModule,
     MatCardModule,
     MatButtonModule,
     MatIconModule,
+    LayoutModule,
   ],
   exports: [
     MainPageComponent,
