1fd3786fbad0303ea0420ff863e120b406b53b01
Roman Kotyshev <roman@MacBook-Air-Roman.local>
2024-01-17 13:57:01 +0300

    refactor: cleanup code, add typography to theme and some styles to novelties

--- src/app/feature/main-page/novelties/novelties.component.ts
+++ src/app/feature/main-page/novelties/novelties.component.ts
@@ -1,74 +1,52 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import { BreakpointObserver, BreakpointState } from '@angular/cdk/layout';
+import { AppBreakpoints, DisplayNameMap } from '../../../utils/constants/layout-constants';
+import { Subject, takeUntil } from 'rxjs';
 
 @Component({
   selector: 'app-novelties',
   templateUrl: './novelties.component.html',
   styleUrl: './novelties.component.scss',
 })
-export class NoveltiesComponent implements OnInit {
+export class NoveltiesComponent implements OnInit, OnDestroy {
   public noveltiesList!: string[];
   public currentScreenSize: string = 'unknown';
-  public noveltiesCountMap = new Map([
-    ['isMVertical', 1],
-    ['isMHorizontal', 3],
-    ['isTablet', 4],
-    ['isDesktop', 5],
+  private _noveltiesCountMap = new Map([
+    ['isMVertical', 4],
+    ['isMHorizontal', 6],
+    ['isTablet', 8],
+    ['isDesktop', 10],
   ]);
-
-  // public displayNameMap = new Map([
-  //   [Breakpoints.XSmall, 'XSmall'],
-  //   [Breakpoints.Small, 'Small'],
-  //   [Breakpoints.Medium, 'Medium'],
-  //   [Breakpoints.Large, 'Large'],
-  //   [Breakpoints.XLarge, 'XLarge'],
-  // ]);
-
-  public displayNameMap = new Map([
-    ['(max-width: 766.98px)', 'isMVertical'],
-    ['(min-width: 767px) and (max-width: 991.98px)', 'isMHorizontal'],
-    ['(min-width: 992px) and (max-width: 1199.98px)', 'isTablet'],
-    ['(min-width: 1200px)', 'isDesktop'],
-  ]);
-
-  public customBreakpoints = {
-    'MVertical': '(max-width: 766.98px)',
-    'MHorizontal': '(min-width: 767px) and (max-width: 991.98px)',
-    'Tablet': '(min-width: 992px) and (max-width: 1199.98px)',
-    'Desktop': '(min-width: 1200px)',
-  };
+  private _destroyed = new Subject<void>();
 
   constructor(public breakpointObserver: BreakpointObserver) {
     breakpointObserver.observe([
-      // Breakpoints.XSmall,
-      // Breakpoints.Small,
-      // Breakpoints.Medium,
-      // Breakpoints.Large,
-      // Breakpoints.XLarge,
-      this.customBreakpoints.MVertical,
-      this.customBreakpoints.MHorizontal,
-      this.customBreakpoints.Tablet,
-      this.customBreakpoints.Desktop,
+      AppBreakpoints.MVertical,
+      AppBreakpoints.MHorizontal,
+      AppBreakpoints.Tablet,
+      AppBreakpoints.Desktop,
     ])
+      .pipe(takeUntil(this._destroyed))
       .subscribe((result: BreakpointState) => {
         for (const query of Object.keys(result.breakpoints)) {
           if(result.breakpoints[query]) {
-            this.currentScreenSize = this.displayNameMap.get(query) ?? 'unknown';
+            this.currentScreenSize = DisplayNameMap.get(query) ?? 'unknown';
             console.log(this.currentScreenSize);
-            this.noveltiesList = ['1', '2', '3', '4', '5']
-              .slice(0, (this.noveltiesCountMap.get(this.currentScreenSize)));
+            this.noveltiesList = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '10', '11', '12']
+              .slice(0, (this._noveltiesCountMap.get(this.currentScreenSize)));
           }
         }
       });
   }
 
   public ngOnInit(): void {
-    // if (this.breakpointObserver.isMatched(['(min-width: 200px)'])) {
-    //   console.log('isMatched');
-    // }
+    this.noveltiesList = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '10', '11', '12']
+      .slice(0, (this._noveltiesCountMap.get(this.currentScreenSize)));
+  }
 
-    this.noveltiesList = ['1', '2', '3', '4', '5']
-      .slice(0, (this.noveltiesCountMap.get(this.currentScreenSize)));
+  public ngOnDestroy(): void {
+    this._destroyed.next();
+    this._destroyed.complete();
   }
 
 }
