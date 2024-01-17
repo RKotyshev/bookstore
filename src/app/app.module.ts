1331183a1cd59eb04df9f0e1a29ef1985e98c8a7
Roman Kotyshev <roman@MacBook-Air-Roman.local>
2024-01-17 17:28:30 +0300

    feat(elements): create footer-toolbar component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,10 +10,12 @@ import { MatIconModule } from '@angular/material/icon';
 import { MatButtonModule } from '@angular/material/button';
 import { MatListModule } from '@angular/material/list';
 import { MainPageModule } from './feature/main-page/main-page.module';
+import { FooterToolbarComponent } from './footer-toolbar/footer-toolbar.component';
 
 @NgModule({
   declarations: [
     AppComponent,
+    FooterToolbarComponent,
   ],
   imports: [
     BrowserModule,
