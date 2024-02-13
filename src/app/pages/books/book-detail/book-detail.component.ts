3ecfa7345048c024b2042ea9393d17cb58462cc4
Roman Kotyshev <roman@MacBook-Air-Roman.local>
2024-02-13 15:27:54 +0300

    refactor: change retrieving data approach in book-detail component to distinct methods

--- src/app/pages/books/book-detail/book-detail.component.ts
+++ src/app/pages/books/book-detail/book-detail.component.ts
@@ -1,14 +1,14 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import { ActivatedRoute, ParamMap } from '@angular/router';
 
-import { EMPTY, Observable, combineLatest, forkJoin, isObservable, map, of, switchMap } from 'rxjs';
+import { Observable, Subject, forkJoin, map, of, shareReplay, switchMap, takeUntil } from 'rxjs';
 
 import { BooksService } from '../../../core/services/books.service';
-import { IBook } from '../../../core/interfaces/book';
 import { AuthorsService } from '../../../core/services/authors.service';
+import { GenresService } from '../../../core/services/genres.service';
+import { IBook } from '../../../core/interfaces/book';
 import { IAuthor } from '../../../core/interfaces/author';
 import { IGenre } from '../../../core/interfaces/genre';
-import { GenresService } from '../../../core/services/genres.service';
 
 
 @Component({
@@ -16,11 +16,16 @@ import { GenresService } from '../../../core/services/genres.service';
   templateUrl: './book-detail.component.html',
   styleUrl: './book-detail.component.scss',
 })
-export class BookDetailComponent implements OnInit {
+export class BookDetailComponent implements OnInit, OnDestroy {
   public book!: IBook;
-  public authors!: IAuthor[];
-  public genres!: IGenre[];
-
+  public authors: IAuthor[] = [];
+  public genres: IGenre[] = [];
+  public book$: Observable<IBook> = this._route.paramMap.pipe(
+    switchMap((paramMap: ParamMap) => this._booksService.getBook(paramMap.get('id')!)),
+    shareReplay({ bufferSize: 1, refCount: true }),
+  );
+  private _destroyed = new Subject<void>();
+  
   constructor(
     private _route: ActivatedRoute,
     private _booksService: BooksService,
@@ -29,91 +34,47 @@ export class BookDetailComponent implements OnInit {
   ) { }
 
   public ngOnInit(): void {
-    this.getData();
+    this.getBook();
+    this.getAuthors();
+    this.getGenres();
   }
 
-  public getData(): void {
-    this._route.paramMap.pipe(
-      switchMap((paramMap: ParamMap) => this._booksService.getBook(paramMap.get('id')!)),
-      map((book: IBook) => {
-        const authors = book.author.map((author: number) => this._authorService.getAuthor(author));
-        const genres = book.genres.map((genre: number) => this._genreService.getGenre(genre));
-        
-        return {
-          book,
-          authors,
-          genres,
-        };
+  public ngOnDestroy(): void {
+    this._destroyed.next();
+    this._destroyed.complete();
+  }
+  
+  public getBook(): void {
+    this.book$.pipe(
+      takeUntil(this._destroyed),
+    )
+      .subscribe((book: IBook) => {
+        this.book = book;
+      });
+  }
+
+  public getAuthors(): void {
+    this.book$.pipe(
+      map((book: IBook) => book.author.map((id: number) => this._authorService.getAuthor(id))),
+      switchMap((authorReqs: Observable<IAuthor>[]) => {
+        return authorReqs.length ? forkJoin(authorReqs) : of(null);
       }),
-      switchMap((
-        { book, authors, genres }:
-          { book: IBook, authors: Observable<IAuthor>[], genres: Observable<IGenre>[]},
-      ) => {
-        const requests: {
-          book: Observable<IBook>
-          authors?: Observable<IAuthor[]>,
-          genres?: Observable<IGenre[]>,
-        } = {
-          book: of(book),
-        };
-
-        if (authors.length) {
-          requests.authors = forkJoin(authors);
-        }
-
-        if (genres.length) {
-          requests.genres = forkJoin(genres);
-        }
-
-
-        return forkJoin(requests);
-      }),
-    ).subscribe((
-      { book, authors, genres }: { book: IBook, authors?: IAuthor[], genres?: IGenre[] },
-    ) => {
-      console.log(book);
-      this.book = book;
+      takeUntil(this._destroyed),
+    ).subscribe((authors: IAuthor[] | null) => {
       this.authors = authors ? authors : [];
-      
-      this.genres = genres ? genres : [];
-      
     });
   }
 
-  // public getData(): void {
-  //   this._route.paramMap.pipe(
-  //     switchMap((paramMap: ParamMap) => this._booksService.getBook(paramMap.get('id')!)),
-  //     map((book: IBook) => {
-
-  //       const authors = book.author.map((author: number) => this._authorService.getAuthor(author));
-  //       const genres = book.genres.map((genre: number) => this._genreService.getGenre(genre));
-        
-
-  //       return {
-  //         book,
-  //         authors,
-  //         genres,
-  //       };
-  //     }),
-  //     switchMap((
-  //       { book, authors, genres }:
-  //         { book: IBook, authors: Observable<IAuthor>[], genres: Observable<IGenre>[] },
-  //     ) => {
-
-  //       return combineLatest({
-  //         book: of(book),
-  //         authors: forkJoin(authors),
-  //         genres: forkJoin(genres),
-  //       });
-  //     }),
-  //   ).subscribe((
-  //     { book, authors, genres }: { book: IBook, authors: IAuthor[], genres: IGenre[] },
-  //   ) => {
-  //     console.log(book);
-  //     this.book = book;
-  //     this.authors = authors;
-  //     this.genres = genres;
-  //   });
-  // }
+  public getGenres(): void {
+    this.book$.pipe(
+      map((book: IBook) => book.genres.map((id: number) => this._genreService.getGenre(id))),
+      switchMap((genreReqs: Observable<IGenre>[]) => {
+        return genreReqs.length ? forkJoin(genreReqs) : of(null);
+      }),
+      takeUntil(this._destroyed),
+    ).subscribe((genres: IGenre[] | null) => {
+      this.genres = genres ? genres : [];
+    });
+  }
 
 }
