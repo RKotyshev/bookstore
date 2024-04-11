0ad94399a7efc6f59d771cbdae2219d2336dddc6
Roman Kotyshev <roman@MacBook-Air-Roman.local>
2024-04-11 14:05:10 +0300

    feat(auth): apply OnPush strategy to auth component, refactor code within auth entities and related files

---
+++ src/app/core/interfaces/auth.ts
@@ -0,0 +1,6 @@
+import { FormControl } from '@angular/forms';
+
+export interface IAuthForm {
+  email: FormControl<string>,
+  password: FormControl<string>,
+}
--- src/app/pages/auth/auth.component.ts
+++ src/app/pages/auth/auth.component.ts
@@ -1,11 +1,51 @@
-import { Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, NonNullableFormBuilder, Validators } from '@angular/forms';
+
+import { IAuthForm } from '../../core/interfaces/auth';
 
 
 @Component({
   selector: 'app-auth',
   templateUrl: './auth.component.html',
   styleUrl: './auth.component.scss',
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class AuthComponent {
+export class AuthComponent implements OnInit {
+  public authForm!: FormGroup<IAuthForm>;
 
+  constructor(
+    private _formBuilder: NonNullableFormBuilder,
+  ) { }
+
+  public get emailControl(): FormControl<string> {
+    return this.authForm.get('email') as FormControl;
+  }
+
+  public get passwordControl(): FormControl<string> {
+    return this.authForm.get('password') as FormControl;
+  }
+
+  public ngOnInit(): void {
+    this._initForm();
+  }
+
+  private _initForm(): void {
+    this.authForm = this._formBuilder.group<IAuthForm>({
+      email: this._formBuilder.control({
+        value: '',
+        disabled: false,
+      }, {
+        validators: [
+          Validators.required,
+          Validators.email,
+        ],
+      }),
+      password: this._formBuilder.control({
+        value: '',
+        disabled: false,
+      }, {
+        validators: [Validators.required],
+      }),
+    });
+  }
 }
