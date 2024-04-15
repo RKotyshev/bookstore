fe35d8b6ad6e2fb41d3c1591358d4c91643e676f
Roman Kotyshev <roman@MacBook-Air-Roman.local>
2024-04-15 15:34:34 +0300

    refactor(adapter): improve interceptor, clean up unnecessary code

--- src/app/core/interceptors/params-adapter.interceptor.ts
+++ src/app/core/interceptors/params-adapter.interceptor.ts
@@ -1,56 +1,60 @@
-import { HttpEvent, HttpHandler, HttpInterceptor, HttpParams, HttpRequest, HttpResponse } from '@angular/common/http';
+import { 
+  HttpEvent,
+  HttpHandler,
+  HttpInterceptor,
+  HttpParams,
+  HttpRequest,
+  HttpResponse,
+} from '@angular/common/http';
 import { Injectable } from '@angular/core';
+
 import { Observable, map } from 'rxjs';
-
-interface IParamsObj {
-  [x: string]: string | null
-}
-
-interface IBodyObj {
-  [x: string]: string | number | object | null, 
-}
 
 
 @Injectable()
 export class ParamsAdapterInterceptor implements HttpInterceptor {
-  public intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
-    // const transformedParams = req.params;
-    // console.log(req.params);
-    // console.log(req.body);
+  public intercept(req: HttpRequest<unknown>, next: HttpHandler): Observable<HttpEvent<unknown>> {
 
-    const paramsObject: IParamsObj = {};
+    const newOutputParams: { [x: string]: unknown } = {};
 
     for (const key of req.params.keys()) {
-      paramsObject[key] = req.params.get(key);
+      newOutputParams[key] = req.params.get(key);
     }
 
-    const cloneBody = req.clone().body;
+    const transformedOutputParams = new HttpParams({
+      fromObject: this.transformParamsOrBody(newOutputParams, 'toClient') as { 
+        [s: string]: string | number 
+      },
+    });
 
-    // let transformedBody: unknown = {};
+    const newOutputBody: object | null = req.body ? { ...req.body } : null;
+    const transformedOutputBody: object | null = newOutputBody ? 
+      this.transformParamsOrBody(newOutputBody, 'toClient') : null;
 
-    if (cloneBody) {
-      // transformedBody = this.transformToClientParams(cloneBody);
-    }
+    const cloneReq = req.clone({
+      params: transformedOutputParams,
+      body: transformedOutputBody,
+    });
 
-    // const transformedParams = this.transformToClientParams(paramsObject);
-
-    // console.log(paramsObject);
-
-    // console.log(`Transformed params = ${JSON.stringify(transformedParams)}`);
-    // console.log(`Transformed body = ${JSON.stringify(transformedBody)}`);
+    console.log('Output: ---------');
+    console.log(cloneReq);
+    console.log('-----------------');
 
     return next.handle(req).pipe(
       map((event: HttpEvent<unknown>) => {
-        console.log('Event from backend:');
-        console.log(event);
-
         if (event instanceof HttpResponse) {
-          console.log('event body:');
-          console.log(event.body);
+          const newInputBody: object | null = event.body ? { ...event.body } : null;
 
-          const transformedServerBody = this.transformToClientParams(event.body);
-          console.log('transformed event body:');
-          console.log(transformedServerBody);
+          const transformedInputBody: object | null = newInputBody ? 
+            this.transformParamsOrBody(newInputBody, 'toClient') : null;
+
+          const cloneEvent = event.clone({
+            body: transformedInputBody,
+          });
+
+          console.log('Input: ---------');
+          console.log(cloneEvent);
+          console.log('-----------------');
         }
 
         return event;
@@ -58,88 +62,63 @@ export class ParamsAdapterInterceptor implements HttpInterceptor {
     );
   }
 
-  public transformToClientParams(inputObj: unknown): unknown {
+  public transformParamsOrBody(
+    inputObj: object | unknown[],
+    direction: 'toClient' | 'toServer',
+  ): object {
     if (Array.isArray(inputObj)) {
       return inputObj.map((value: unknown) => {
-        if (typeof value === 'object') {
-          return this.transformToClientParams(value);
+
+        if (typeof value === 'object' && value !== null) {
+          return this.transformParamsOrBody(value, direction);
         }
 
         return value;
       });
     }
 
-    if (typeof inputObj === 'object' && inputObj !== null && inputObj !== undefined) {
-      return Object.entries(inputObj).reduce(
-        (resultObject: { [x: string]: unknown }, [key, value]: [string, unknown]) => {
-          if (typeof value === 'object') {
-            value = this.transformToClientParams(value);
-          }
-    
-          if (!key.includes('_')) {
-            resultObject[key] = value;
-    
-            return resultObject;
-          }
-    
-          while (key.includes('_')) {
-            const dashIndex = key.indexOf('_');
-            const beforeDash = key.slice(0, dashIndex);
-            const afterDash = key.slice(dashIndex + 1)[0].toUpperCase() + key.slice(dashIndex + 2);
-            key = beforeDash + afterDash;
-          }
-    
-          resultObject[key] = value;
-    
-          return resultObject;
-        }, {});
-    }
+    return Object.entries(inputObj).reduce(
+      (resultObject: { [x: string]: unknown }, [key, value]: [string, unknown]) => {
+
+        if (typeof value === 'object' && value !== null) {
+          value = this.transformParamsOrBody(value, direction);
+        }
+
+        const transformedKey = this.transformKey(key, direction);
+  
+        resultObject[transformedKey] = value;
+  
+        return resultObject;
+      }, {});
   }
 
-  public transformToServerParams(inputObj: unknown): unknown {
-    if (Array.isArray(inputObj)) {
-      // console.log('test');
+  public transformKey(key: string, direction: 'toClient' | 'toServer'): string  {
+    if (direction === 'toClient') {
 
-      return inputObj.map((value: unknown) => {
-        if (typeof value === 'object') {
-          console.log('value is object');
+      if (!key.includes('_')) {
+        return key;
+      }
 
-          return this.transformToServerParams(value);
-        }
-  
-        return value;
-      });
+      while (key.includes('_')) {
+        const dashIndex = key.indexOf('_');
+        const beforeDash = key.slice(0, dashIndex);
+        const afterDash = key.slice(dashIndex + 1)[0].toUpperCase() + key.slice(dashIndex + 2);
+        key = beforeDash + afterDash;
+      }
+
+      return key;
+    }
+
+    if (!(/[A-Z]/.test(key))) {
+      return key;
+    }
+
+    while (/[A-Z]/.test(key)) {
+      const match = key.match(/[A-Z]/);
+      key = `${key.slice(0, match?.index)} _  ${match![0]
+        .toLowerCase()} ${key.slice(match!.index! + 1)}`;
     }
     
-    if (typeof inputObj === 'object' && inputObj !== null && inputObj !== undefined) {
-      // console.log('test');
-
-      return Object.entries(inputObj).reduce(
-        (resultObject: { [x: string]: unknown }, [key, value]: [string, unknown]) => {
-          if (typeof value === 'object') {
-            value = this.transformToServerParams(value);
-          }
-      
-          if (!(/[A-Z]/.test(key))) {
-            resultObject[key] = value;
-            console.log(`key = ${key}`);
-      
-            return resultObject;
-          }
-      
-          while (/[A-Z]/.test(key)) {
-            const match = key.match(/[A-Z]/);
-            key = `${key.slice(0, match?.index)} _  ${match![0]
-              .toLowerCase()} ${key.slice(match!.index! + 1)}`;
-          }
-      
-          resultObject[key] = value;
-          
-          console.log('test');
-
-          return resultObject;
-        }, {});
-    }
-
+    return key;
   }
 }
